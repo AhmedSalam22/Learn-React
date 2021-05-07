6d09efe533b0f046b1f15c7322e5f4ef9f8e25cc
Ahmed Maher Salam <ahmedmahersalam22@gmail.com>
2021-05-07 17:04:47 +0200

    configure React

---
+++ React/learn_react/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">
+              BIS
+          </NavbarBrand>
+        </div>
+          
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
